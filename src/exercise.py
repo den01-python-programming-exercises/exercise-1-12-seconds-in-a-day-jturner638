ba3fad4727120451602f8e285204d3a4888bf0b9
Justin Turner <mrfathed@protonmail.com>
2021-12-04 09:46:07 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
     #write your code below this line
+    num_of_days = int(input("How many days would you like to convert to seconds?"))
+    print(num_of_days * 86400)
 
 if __name__ == '__main__':
     main()
